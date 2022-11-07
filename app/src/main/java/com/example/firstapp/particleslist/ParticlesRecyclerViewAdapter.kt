70fc9046fd5c9a1d43252ce744d11e56453942be
orodrigzz <oscarrod18503@gmail.com>
2022-11-07 19:12:44 +0100

    Bloc de Notas 7 Noviembre

--- app/src/main/java/com/example/firstapp/particleslist/ParticlesRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/firstapp/particleslist/ParticlesRecyclerViewAdapter.kt
@@ -1,9 +1,10 @@
 package com.example.firstapp.particleslist
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapp.MainActivity
 import com.example.firstapp.databinding.ItemParticleBinding
 
 class ParticlesRecyclerViewAdapter(val particles: List<String>) :
@@ -14,13 +15,22 @@ class ParticlesRecyclerViewAdapter(val particles: List<String>) :
 
         val name = binding.particleName
         val image = binding.lapizImage
-        val delete = binding.deleteButton
+        val deleteBtn = binding.deleteButton
         val layout = binding.notaLayout
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParticleVH {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemParticleBinding.inflate(layoutInflater)
+
+        binding.deleteButton.setOnClickListener {
+            // Toast.makeText(this, "Hola classe", Toast.LENGTH_LONG).show()
+        }
+
+        binding.notaLayout.setOnClickListener {
+            //val intent = Intent(this, MainActivity::class.java)
+            //startActivity(intent)
+        }
         return ParticleVH(binding)
     }
 
