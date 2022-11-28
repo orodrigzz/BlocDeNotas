894f6bcb5c8d81a23e670a1adccecdd07ffafb04
orodrigzz <oscarrod18503@gmail.com>
2022-11-28 18:54:38 +0100

    Base del fuego

--- app/src/main/java/com/example/firstapp/particleslist/Particle.kt
+++ app/src/main/java/com/example/firstapp/particleslist/Particle.kt
@@ -1,6 +1,10 @@
 package com.example.firstapp.particleslist
 
-class Particle (var name : String, val family : Familiy ,val mass : String, val charge : String, val spin : String){
+data class Particle (var name : String,
+                     val family : Familiy,
+                     val mass : String,
+                     val charge : String,
+                     val spin : String){
 
     enum class Familiy{
         QUARK, LEPTON, GAGUE_BOSSOM, HIGH_BOSSOM
