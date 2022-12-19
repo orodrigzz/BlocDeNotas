c2ae1fc3e18595877c678eb2fb519c1e790e2077
orodrigzz <oscarrod18503@gmail.com>
2022-12-19 16:53:57 +0100

    Ads

--- app/src/main/java/com/example/firstapp/NotesActivity.kt
+++ app/src/main/java/com/example/firstapp/NotesActivity.kt
@@ -1,14 +1,22 @@
 package com.example.firstapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firstapp.databinding.ActivityNotesBinding
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class NotesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNotesBinding
     private val llmanager = LinearLayoutManager(this)
+    private var ad: InterstitialAd? = null
 
     private var notes = listOf(
         "NotaPredeterminada1",
@@ -35,6 +43,10 @@ class NotesActivity : AppCompatActivity() {
 
         binding.particlesRecyclerView.adapter = NotesRecyclerViewAdapter(lista,this)
 
+        MobileAds.initialize(this)
+
+        val somethif = this
+
         binding.addButton.setOnClickListener {
             createNota()
         }
@@ -49,5 +61,52 @@ class NotesActivity : AppCompatActivity() {
         binding.particlesRecyclerView.adapter?.notifyItemInserted(3)
         llmanager.scrollToPositionWithOffset(3,10)
 
+    }
+    override fun onResume() {
+        super.onResume()
+
+        val request = AdRequest.Builder().build()
+        binding.adView.loadAd(request)
+        binding.adView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                super.onAdClicked()
+            }
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+            }
+        }
+
+        loadAd(this)
+    }
+
+    fun loadAd(activity: Activity) {
+        val request = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            activity,
+            "ca-app-pub-3940256099942544/1033173712",
+            request,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(loadedAd: InterstitialAd) {
+                    ad = loadedAd
+                }
+            })
     }
 }
--- app/src/main/java/com/example/firstapp/NotesRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/firstapp/NotesRecyclerViewAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.firstapp
 
+import android.annotation.SuppressLint
 import android.content.Context
-//import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firstapp.databinding.ItemNoteBinding
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.interstitial.InterstitialAd
 
 class NotesRecyclerViewAdapter(private val lista: MutableList<String>, private val context : Context) :
     RecyclerView.Adapter<NotesRecyclerViewAdapter.NoteVH>() {
+
+    private var ad: InterstitialAd? = null
 
     inner class NoteVH(binding: ItemNoteBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -25,12 +30,27 @@ class NotesRecyclerViewAdapter(private val lista: MutableList<String>, private v
         return NoteVH(binding)
     }
 
-    override fun onBindViewHolder(holder: NoteVH, position: Int) {
+    override fun onBindViewHolder(holder: NoteVH, @SuppressLint("RecyclerView") position: Int) {
         val note = lista[position]
         holder.name.text = note
 
         holder.deleteBtn.setOnClickListener {
-            lista.removeAt(position)
+
+            ad?.let {
+                it.fullScreenContentCallback = object : FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        lista.removeAt(position)
+                        notifyItemRemoved(position)
+                        ad = null
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                        lista.removeAt(position)
+                        notifyItemRemoved(position)
+                        ad = null
+                    }
+                }
+            }?:lista.removeAt(position)
             notifyItemRemoved(position)
         }
 
